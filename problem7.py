a5f23e89bf3a004dd5bcbe39d631ae3500c50e28
charlie <sadegh.rez@gmail.com>
2021-10-05 19:32:04 +0330

    problem7

---
+++ problem7.py
@@ -0,0 +1,19 @@
+"""
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+What is the 10 001st prime number?
+"""
+
+a = [2]
+x = 3
+
+while len(a) < 10001 :
+    flag = False
+    for d in a :
+        if x % d == 0 :
+            flag = True
+            break
+    if flag == False :
+        a.append(x)
+    x += 1
+
+print(a[-1])
