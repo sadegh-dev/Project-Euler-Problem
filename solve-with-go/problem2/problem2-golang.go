d9619c5ba25d03742f0655644e3c6de950757077
sadegh-dev <sadegh.rez@gmail.com>
2023-12-18 23:27:17 +0330

    # problem2-with_GO / if statement, check high number

--- solve-with-go/problem2/problem2-golang.go
+++ solve-with-go/problem2/problem2-golang.go
@@ -3,9 +3,17 @@ package problem2
 func main() {
 
 	fibo := []int{1, 2}
+	index := 1
+	new := 0
 
 	for true {
-		//pass
+		new = fibo[index] + fibo[index-1]
+
+		if new > 100 {
+			break
+		} else {
+			fibo = append(fibo, new)
+		}
 	}
 
 }
