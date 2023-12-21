237cc3094cecf359865998186057409ef979b822
sadegh-dev <sadegh.rez@gmail.com>
2023-12-21 22:58:15 +0330

    if statement for checking even number and plus by total variable

--- solve-with-go/problem2/problem2-golang.go
+++ solve-with-go/problem2/problem2-golang.go
@@ -7,16 +7,20 @@ func main() {
 	fibo := []int{1, 2}
 	index := 1
 	new := 0
-	total := 0
+	total := 2
 
 	for true {
 		new = fibo[index] + fibo[index-1]
 
-		if new > 1000 {
+		if new > 10 {
 			break
 		} else {
 			fibo = append(fibo, new)
 			index += 1
+
+			if new%2 == 0 {
+				total += new
+			}
 		}
 	}
 	fmt.Println(fibo)
